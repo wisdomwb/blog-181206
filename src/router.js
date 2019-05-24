89d749131ced59047807f999ee7b51daace96fec
weizhen <a610428@qq.com>
2019-05-24 08:32:50 +0800

    mod router

--- src/router.js
+++ src/router.js
@@ -18,6 +18,7 @@ export default new Router({
       path: '/',
       name: 'index',
       component: CommonLayout,
+      redirect: '/home',
       children: [{
         path: 'home',
         name: 'home',
